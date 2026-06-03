40421c92629c2730903261af4d2a1403215e2efd
SvetlanaShkryleva <114089663+SvetlanaShkryleva@users.noreply.github.com>
2026-06-03 10:11:23 +0300

    fix

--- test/tests.cpp
+++ test/tests.cpp
@@ -12,60 +12,60 @@ const double EPS = 0.001;
 // ТЕСТЫ КЛАССА CIRCLE
 // -----------------------------
 
-TEST(CircleTest, ConstructorRadius5) {
-  Circle c(5.0);
-  EXPECT_NEAR(5.0, c.getRadius(), EPS);
+TEST(CircleTest, ConstructorRadius3) {
+  Circle c(3.0);
+  EXPECT_NEAR(3.0, c.getRadius(), EPS);
 }
 
-TEST(CircleTest, ConstructorRadius12) {
-  Circle c(12.0);
-  EXPECT_NEAR(12.0, c.getRadius(), EPS);
+TEST(CircleTest, ConstructorRadius7) {
+  Circle c(7.0);
+  EXPECT_NEAR(7.0, c.getRadius(), EPS);
 }
 
 TEST(CircleTest, RadiusUpdatesFerence) {
   Circle c(0);
-  c.setRadius(4.0);
-  EXPECT_NEAR(25.1327, c.getFerence(), EPS);
+  c.setRadius(3.0);
+  EXPECT_NEAR(18.8496, c.getFerence(), EPS);
 }
 
 TEST(CircleTest, RadiusUpdatesArea) {
   Circle c(0);
-  c.setRadius(4.0);
-  EXPECT_NEAR(50.2654, c.getArea(), EPS);
+  c.setRadius(3.0);
+  EXPECT_NEAR(28.2743, c.getArea(), EPS);
 }
 
 TEST(CircleTest, FerenceUpdatesRadius) {
   Circle c(0);
-  c.setFerence(31.4159);
-  EXPECT_NEAR(5.0, c.getRadius(), EPS);
+  c.setFerence(18.8496);
+  EXPECT_NEAR(3.0, c.getRadius(), EPS);
 }
 
 TEST(CircleTest, FerenceUpdatesArea) {
   Circle c(0);
-  c.setFerence(31.4159);
-  EXPECT_NEAR(78.5398, c.getArea(), EPS);
+  c.setFerence(18.8496);
+  EXPECT_NEAR(28.2743, c.getArea(), EPS);
 }
 
 TEST(CircleTest, AreaUpdatesRadius) {
   Circle c(0);
-  c.setArea(314.159);
-  EXPECT_NEAR(10.0, c.getRadius(), EPS);
+  c.setArea(78.5398);
+  EXPECT_NEAR(5.0, c.getRadius(), EPS);
 }
 
 TEST(CircleTest, AreaUpdatesFerence) {
   Circle c(0);
-  c.setArea(314.159);
-  EXPECT_NEAR(62.8318, c.getFerence(), EPS);
+  c.setArea(78.5398);
+  EXPECT_NEAR(31.4159, c.getFerence(), EPS);
 }
 
 TEST(CircleTest, SmallRadiusValue) {
-  Circle c(0.01);
+  Circle c(0.05);
   EXPECT_GT(c.getArea(), 0.0);
 }
 
 TEST(CircleTest, VeryLargeRadius) {
-  Circle c(100000);
-  EXPECT_GT(c.getFerence(), 600000);
+  Circle c(50000);
+  EXPECT_GT(c.getFerence(), 300000);
 }
 
 TEST(CircleTest, MultipleChanges) {
@@ -76,7 +76,7 @@ TEST(CircleTest, MultipleChanges) {
 }
 
 TEST(CircleTest, AreaPositive) {
-  Circle c(8);
+  Circle c(2);
   EXPECT_GT(c.getArea(), 0);
 }
 
@@ -84,34 +84,34 @@ TEST(CircleTest, AreaPositive) {
 // ТЕСТЫ ЗАДАЧИ "ЗЕМЛЯ И ВЕРЕВКА"
 // -----------------------------
 
-TEST(EarthTaskTest, OneMeterGap) {
-  double gap = calculateEarthGap(6378.1, 1.0);
-  EXPECT_NEAR(0.15915, gap, 0.001);
+TEST(EarthTaskTest, ThreeMeterGap) {
+  double gap = calculateEarthGap(6378.1, 3.0);
+  EXPECT_NEAR(0.47746, gap, 0.001);
 }
 
-TEST(EarthTaskTest, TwoMeterGap) {
-  double gap = calculateEarthGap(6378.1, 2.0);
-  EXPECT_NEAR(0.31831, gap, 0.001);
+TEST(EarthTaskTest, FourMeterGap) {
+  double gap = calculateEarthGap(6378.1, 4.0);
+  EXPECT_NEAR(0.63662, gap, 0.001);
 }
 
-TEST(EarthTaskTest, FiveMeterGap) {
-  double gap = calculateEarthGap(6378.1, 5.0);
-  EXPECT_NEAR(0.79577, gap, 0.001);
+TEST(EarthTaskTest, TenMeterGap) {
+  double gap = calculateEarthGap(6378.1, 10.0);
+  EXPECT_NEAR(1.59155, gap, 0.001);
 }
 
 TEST(EarthTaskTest, SmallPlanetGap) {
-  double gap = calculateEarthGap(100.0, 1.0);
-  EXPECT_NEAR(0.15915, gap, 0.001);
+  double gap = calculateEarthGap(200.0, 3.0);
+  EXPECT_NEAR(0.47746, gap, 0.001);
 }
 
 TEST(EarthTaskTest, LargePlanetGap) {
-  double gap = calculateEarthGap(20000.0, 1.0);
-  EXPECT_NEAR(0.15915, gap, 0.001);
+  double gap = calculateEarthGap(50000.0, 3.0);
+  EXPECT_NEAR(0.47746, gap, 0.001);
 }
 
-TEST(EarthTaskTest, HundredMetersAdded) {
-  double gap = calculateEarthGap(6378.1, 100.0);
-  EXPECT_NEAR(15.9155, gap, 0.001);
+TEST(EarthTaskTest, FiftyMetersAdded) {
+  double gap = calculateEarthGap(6378.1, 50.0);
+  EXPECT_NEAR(7.95775, gap, 0.001);
 }
 
 TEST(EarthTaskTest, ZeroAddedLength) {
@@ -124,31 +124,31 @@ TEST(EarthTaskTest, ZeroAddedLength) {
 // -----------------------------
 
 TEST(PoolTaskTest, StandardRoadCost) {
-  Pool res = calculateCostOfPool(3.0, 1.0);
-  EXPECT_NEAR(21991.14, res.costOfRoad, 0.5);
+  Pool res = calculateCostOfPool(4.0, 2.0);
+  EXPECT_NEAR(62831.85, res.costOfRoad, 0.5);
 }
 
 TEST(PoolTaskTest, StandardFenceCost) {
-  Pool res = calculateCostOfPool(3.0, 1.0);
-  EXPECT_NEAR(50265.48, res.costOfFence, 0.5);
+  Pool res = calculateCostOfPool(4.0, 2.0);
+  EXPECT_NEAR(75398.22, res.costOfFence, 0.5);
 }
 
 TEST(PoolTaskTest, NoPathCost) {
-  Pool res = calculateCostOfPool(4.0, 0.0);
+  Pool res = calculateCostOfPool(5.0, 0.0);
   EXPECT_NEAR(0.0, res.costOfRoad, EPS);
 }
 
 TEST(PoolTaskTest, SmallPoolCost) {
-  Pool res = calculateCostOfPool(1.0, 0.5);
+  Pool res = calculateCostOfPool(1.0, 0.3);
   EXPECT_GT(res.costOfRoad, 0);
 }
 
 TEST(PoolTaskTest, FenceCostPositive) {
-  Pool res = calculateCostOfPool(3.0, 1.0);
+  Pool res = calculateCostOfPool(2.0, 1.0);
   EXPECT_GT(res.costOfFence, 0);
 }
 
 TEST(PoolTaskTest, LargePoolFence) {
-  Pool res = calculateCostOfPool(10.0, 2.0);
-  EXPECT_GT(res.costOfFence, 70000);
+  Pool res = calculateCostOfPool(8.0, 1.5);
+  EXPECT_GT(res.costOfFence, 100000);
 }
